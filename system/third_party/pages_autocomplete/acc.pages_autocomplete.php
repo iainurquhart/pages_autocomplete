a11ae72a3cdfcf32f388a9ed92abba90b2efbf14
Iain Urquhart <iain@jamdigital.co.nz>
2011-04-23 21:09:06 +1200

    Restrict height of autocomplete options, force open on click instead of type, prompt the user to enter the last segment which defaults to url_title if the value exists

--- system/third_party/pages_autocomplete/acc.pages_autocomplete.php
+++ system/third_party/pages_autocomplete/acc.pages_autocomplete.php
@@ -13,7 +13,7 @@ class Pages_autocomplete_acc {
 
 	var $name			= 'Pages Autocomplete';
 	var $id				= 'pages_autocomplete';
-	var $version		= '1.0';
+	var $version		= '1.0.1';
 	var $description	= 'Adds autocomplete action to pages_uri field';
 	var $sections		= array();
 
@@ -49,6 +49,8 @@ class Pages_autocomplete_acc {
 				// build our js list of pages for autocompete
 				foreach($pages as $page_uri)
 				{
+					// remove trailing slashes if they exist, as we force them on below
+					$page_uri = rtrim($page_uri, '/');
 					$search_array .= '"'.$page_uri;
 					
 					// add the trailing slash, but not to the home page if it's just '/'
@@ -67,13 +69,44 @@ class Pages_autocomplete_acc {
 			$this->EE->cp->add_to_head('
 			<script type="text/javascript">
 				$(function() {
-				var available_pages = [
-					'.$search_array.'
-				];
-				$("input#pages__pages_uri").autocomplete({
-					source: available_pages
+					var available_pages = [
+						'.$search_array.'
+					];
+										
+					var last_seg = "";
+					$("input#pages__pages_uri").autocomplete({
+						source: available_pages,
+						minLength: 0,
+						delay: 0,
+						open: function(event, ui)
+						{
+							$(".ui-autocomplete").prepend("<li class=\"pages_select_parent_label\">Select a parent, type to filter:</li>");
+						},
+						select: function(event, ui)
+						{
+							var existing_uri = $("#pages__pages_uri").val();
+							if(existing_uri != "/example/pages/uri/")
+							{
+								var url_title = $("input#url_title").val();
+								var last_seg = prompt("Final url segment for this page:",url_title);
+								ui.item.value = ui.item.value + last_seg;
+							}
+						}
+					});
+					
+					// force the select options to open on click
+					$("input#pages__pages_uri").click(function() {
+						
+						var existing_uri = $("#pages__pages_uri").val();
+						if(existing_uri == "")
+						{
+							$(this).val("/").trigger("keydown");
+						}
+					
+					});
+					
+					
 				});
-			});
 			
 			</script>
 			<style type="text/css">
@@ -87,11 +120,14 @@ class Pages_autocomplete_acc {
 				-moz-box-shadow: 0 0 10px rgba(0,0,0,0.3);
 				-o-box-shadow: 0 0 10px rgba(0,0,0,0.3);
 				box-shadow: 0 0 10px rgba(0,0,0,0.3);
+				 height: 200px; overflow-y: scroll; overflow-x: hidden;
 				}	
 				
 				/* workarounds */
 				* html .ui-autocomplete { width:1px; } /* without this, the menu expands to 100% in IE6 */
-
+				
+				.pages_select_parent_label { padding: 5px 3px; color: #bababa;}
+				
 				.ui-menu {
 					list-style:none;
 					padding: 2px;
@@ -117,6 +153,7 @@ class Pages_autocomplete_acc {
 					line-height:1.5;
 					zoom:1;
 				}
+				.ui-menu .ui-menu-item a:hover {cursor:pointer}
 				.ui-menu .ui-menu-item a.ui-state-hover,
 				.ui-menu .ui-menu-item a.ui-state-active {
 					font-weight: normal;
